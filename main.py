42ae41547bed3b3d63dd572ed0696ca4009a5085
Francis Moreta <60371565+francismoretareyes@users.noreply.github.com>
2025-12-09 19:16:41 +0100

    repte e completat

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+MAX_TEMPERATURE = 50
 
+x=2
+y=2
+
+while True:
+    if input.button_is_pressed(Button.A):
+        while True:
+            temp = input.temperature()
+            led.plot_bar_graph(temp, MAX_TEMPERATURE)
+    if input.button_is_pressed(Button.B):
+        while True:
+            led.plot(x, y)
+            basic.pause(50)
+            led.unplot(x, y)
+            accx = input.acceleration(Dimension.X)
+            accy = input.acceleration(Dimension.Y)
+            if accx <= 150 and x > 0:
+                x=x-1
+            if accx > 150 and x < 4:
+                x=x+1
+            if accy <= 150 and y > 0:
+                y=y-1
+            if accy > 150 and y < 4:
+                y=y+1
--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-
+let temp: number;
+let accx: number;
+let accy: number;
+let MAX_TEMPERATURE = 50
+let x = 2
+let y = 2
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        while (true) {
+            temp = input.temperature()
+            led.plotBarGraph(temp, MAX_TEMPERATURE)
+        }
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        while (true) {
+            led.plot(x, y)
+            basic.pause(50)
+            led.unplot(x, y)
+            accx = input.acceleration(Dimension.X)
+            accy = input.acceleration(Dimension.Y)
+            if (accx <= 150 && x > 0) {
+                x = x - 1
+            }
+            
+            if (accx > 150 && x < 4) {
+                x = x + 1
+            }
+            
+            if (accy <= 150 && y > 0) {
+                y = y - 1
+            }
+            
+            if (accy > 150 && y < 4) {
+                y = y + 1
+            }
+            
+        }
+    }
+    
+}
